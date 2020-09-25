d10cc112f246ce4ce51b0e1e461040c978f4081c
EhrenW1 <ehren.wargo@patelhs.org>
2020-09-25 19:18:16 +0000

    I added variables for all my age, my birthday, todays date, and another one for my age if today is my birthday. I made an if statement that said my ag was 16, it only would say i am 16 if todays dat was the same as my birthday. I also made an else statement that simply said i was still 15 due to the fact that todays date and my birthday were not the same. All and all my code simply states how old you are based on your current age, your birthday, and todays date.

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
-class Main {
+class CheckMyAge {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+   int age= 15;
+   int age2= age+1;
+   int myBirthday= 617;
+   int todaysDate= 921;
+   // inserted variable for age, my birthday, and todays date.
+   // Had to add another age variable for age because I could not get age to add one to itself in my print statment.
+   if (myBirthday == todaysDate)
+   //made if statment for id birthday is eaqual to todays date
+   {
+     System.out.println("If my birthday is " +myBirthday+ " and todays date is " +todaysDate+ " I am "  + age2 + " years old.");
+     //wrote line of code that tells age if birthday is today, did this by adding another variable that add 1 to variable age.
+    }else{
+     System.out.println("Since my birth day is " +myBirthday+ " and today is " +todaysDate+ " I am still " +age+ " years old ");
+     // else stament allows for a line to be printed saying i am still 15 if todaysdate is not the same as my birthday
+   }
+   }
   }
-}
